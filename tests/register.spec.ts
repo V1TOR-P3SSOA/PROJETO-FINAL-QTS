a81c1862498ad368c47cd5904a146aab35181d41
V1TOR-P3SSOA <vitor.freitas28@aluno.ce.gov.br>
2026-05-10 15:39:20 -0300

    Adicionar teste para registro de usuário com senhas não correspondentes

--- tests/register.spec.ts
+++ tests/register.spec.ts
@@ -15,6 +15,13 @@ test('User cannot register with existing email', async ({ page }) => {
     await expect(page.getByText('Este e-mail já está cadastrado.')).toBeVisible();
 });
 
+test('User cannot register with mismatched passwords', async ({ page }) => {
+
+    const registerPage = new RegisterPage(page);
+    await registerPage.register('user3', 'user3@gmail.com','000000002', 'S/N', '12345678', '87654321');
+    await expect(page.getByText('As senhas não conferem.')).toBeVisible();
+});
+
 test('User cannot register with empty fields', async ({ page }) => {
 
     const registerPage = new RegisterPage(page);
